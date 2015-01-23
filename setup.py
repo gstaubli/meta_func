293b0666fe2cc80593a3d229bd6cac68b3b9113c
Garren Staubli <gstaubli@demandbase.com>
2015-01-23 12:10:26 -0800

    Leftover

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     author='Garren Staubli',
     author_email='gstaubli@gmail.com',
-    packages=['meta_func','meta_func.test'],
+    packages=['meta_func'],
     url='http://www.github.com/gstaubli/meta_func',
     description='Python decorator function to track metadata on function calls',
     long_description=open('README.md').read()
